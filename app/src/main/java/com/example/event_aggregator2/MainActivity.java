2db21469e94459cc19d1cbbe11ffc790f2a42eae
nikitakosh <nikirofler@gmail.com>
2023-03-22 11:51:20 +0300

    add "hello world"

--- app/src/main/java/com/example/event_aggregator2/MainActivity.java
+++ app/src/main/java/com/example/event_aggregator2/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, EventHome.class);
         startActivity(intent);
+        System.out.println("hello world!!!");
     }
 }
