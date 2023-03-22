2301c23f7ae925c71d9b5947906581a4c30f5281
nikitakosh <nikirofler@gmail.com>
2023-03-22 11:46:43 +0300

    test commit

---
+++ app/src/main/java/com/example/event_aggregator2/Registration.java
@@ -0,0 +1,16 @@
+package com.example.event_aggregator2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+    }
+}
